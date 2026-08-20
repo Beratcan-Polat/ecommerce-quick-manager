935d60818f4da0670a6ea9cc4678d7bb340df0d4
Beratcan Polat <beratcannpolat@gmail.com>
2026-08-20 11:07:07 +0300

    feat: ürün CRUD rotaları tanımlandı

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+
